c4617b882a3c0b45d48fdb03d5cfa8d86ecb41fb
Agnete Moos <agms@sonderborg.dk>
2019-12-16 10:46:46 +0100

    Has sideeffect - logout with no promt - remove

--- remove-shutdown.sh
+++ remove-shutdown.sh
@@ -2,12 +2,7 @@
 
 # Creates a script and saves it in /etc/profile.d. The script removes shutdown from the from the user switcher menu
 
-cat > /etc/profile.d/bibsdb-remove-shutdown.sh << EOF
-#!/usr/bin/env bash
-# This script will run at boot. It removes shutdown from the from the user switcher menu
-gsettings set com.canonical.indicator.session suppress-shutdown-menuitem true
-gsettings set com.canonical.indicator.session suppress-logout-restart-shutdown true
-EOF
+rm -f /etc/profile.d/bibsdb-remove-shutdown.sh
 
 
 
