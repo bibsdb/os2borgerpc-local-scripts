ca4eb70940e46dade516b9cf3779c968f000d621
Agnete Moos <dkagms@dkagms-vm.localdomain>
2020-02-17 14:26:02 +0100

    Mute audio v2

--- mute-audio.sh
+++ mute-audio.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Mute audio
+# Do it now
+amixer set Master mute
+
+# Preserve setting even when automatic cleanup is enabled
 cat > /etc/profile.d/bibsdb-mute-audio.sh << EOF
 #!/usr/bin/env bash
 amixer set Master mute
