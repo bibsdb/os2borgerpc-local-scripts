e68718bd29b47c32a0fc49bd5fc7d7ae4dbc88fb
Agnete Moos <agms@sonderborg.dk>
2020-02-26 11:18:13 +0100

    Sleep 15 and apply to user

--- rotate-screen-and-chrome-kiosk.sh
+++ rotate-screen-and-chrome-kiosk.sh
@@ -14,14 +14,11 @@ rotation=$(get_bibos_config rotation || echo "normal")
 
 
 # Create exec script for Chrome
-cat > /home/.skjult/.chrome.sh << EOF
+cat > /home/user/.chrome.sh << EOF
 #!/usr/bin/env bash
 
 # Rotate screen
 xrandr -o $rotation
-
-# Sleep 20 seconds. Leaves time to establish network connection.
-sleep 20
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/user/.config/google-chrome/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' /home/user/.config/google-chrome/Default/Preferences
@@ -29,11 +26,11 @@ sed -i 's/"restore_on_startup":[0-9]/"restore_on_startup":0/' /home/user/.config
 google-chrome --kiosk $site --full-screen --password-store=basic
 EOF
 
-chmod +x /home/.skjult/.chrome.sh
+chmod +x /home/user/.chrome.sh
 
 
 # Make the Chrome-script autostart
-autostart_dir=/home/.skjult/.config/autostart
+autostart_dir=/home/user/.config/autostart
 
 if [ ! -d "$autostart_dir" ]
 then
@@ -45,7 +42,7 @@ rm -f $autostart_dir/*chrome*
 cat <<CHROME-DESKTOP > $autostart_dir/chrome.desktop
 [Desktop Entry]
 Type=Application
-Exec=/home/user/.chrome.sh
+Exec=sleep 15 && /home/user/.chrome.sh
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
