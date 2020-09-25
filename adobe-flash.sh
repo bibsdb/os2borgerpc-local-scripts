f38a3c4eacb1b6b728ba51c4f7e0e21d3a7972ef
Agnete Moos <agms@sonderborg.dk>
2020-09-25 13:48:47 +0200

    Delete flash script

--- adobe-flash.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Adobe Flash plugin
-
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-sudo apt update
-sudo apt -y install flashplugin-installer
-
-
