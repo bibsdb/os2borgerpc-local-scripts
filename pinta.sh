58b49f9bec773b774a8f927cb1023205b0002f55
Agnete Moos <agms@sonderborg.dk>
2020-09-25 13:49:38 +0200

    Use default pinta from Ubuntu repos

--- pinta.sh
+++ pinta.sh
@@ -2,7 +2,8 @@
 
 # Install Pinta Project
 # Pinta is a free, open source program for drawing and image editing.
-sudo add-apt-repository -y ppa:pinta-maintainers/pinta-stable
-sudo apt-get update
+sudo rm -rf /etc/apt/sources.list.d/pinta*
+apt-get update
+sudo apt-get remove -y pinta
 sudo apt-get install -y pinta
 
