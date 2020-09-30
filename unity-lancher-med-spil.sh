c08846a47ce9d3899c8e8e3d86040dab2e82d7ad
Agnete Moos <agms@sonderborg.dk>
2020-09-30 10:21:50 +0200

    Add firefox browser to menu

--- unity-lancher-med-spil.sh
+++ unity-lancher-med-spil.sh
@@ -2,5 +2,5 @@
 cat > /etc/profile.d/bibsdb-default-apps-unity-launcher.sh << EOF
 #!/usr/bin/env bash
 # Set default applications in unity launcher
-gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://org.gnome.Nautilus.desktop', 'application://minecraft-launcher.desktop', 'application://libreoffice-writer.desktop', 'application://libreoffice-calc.desktop', 'application://libreoffice-impress.desktop', 'application://pinta.desktop', 'application://logout.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://firefox.desktop', 'application://org.gnome.Nautilus.desktop', 'application://minecraft-launcher.desktop', 'application://libreoffice-writer.desktop', 'application://libreoffice-calc.desktop', 'application://libreoffice-impress.desktop', 'application://pinta.desktop', 'application://logout.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
 EOF
