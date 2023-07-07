acb541a205044e60bf59f7413ca028f670ed7739
Agnete Moos <agms@sonderborg.dk>
2023-07-07 08:45:49 +0200

    Bon print queue

--- 2004clear-printer-queue.sh
+++ 2004clear-printer-queue.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-# Clear the print queue for the printer bon
+# Clear the print queue for the printer bon if there are errors 
+# in the CUPS error log. Then clear the error log.
+# Runs once pr. minute.
 cat <<CLEAR-QUEUE > /home/superuser/bibbox-clear-bon-queue.sh
 #!/usr/bin/env bash
-cancel -a bon
+# Empty print queue if there are arrors in the CUPS error log
+# The clear the CUPS error log
+
+if grep "E \[" /var/log/cups/error_log; then
+  echo "The are errors in /var/log/cups/error_log"
+  cancel -a bon
+  echo "Print queue has been emptied"
+  > /var/log/cups/error_log
+  echo "The cups error log has been emptied"
+fi
 CLEAR-QUEUE
 
 chmod +x /home/superuser/bibbox-clear-bon-queue.sh
@@ -16,11 +27,12 @@ if [ "$ACTIVATE" = 'False' ]
 then
   if [ -f $TCRON ]
   then
-    sed -i -e "/clear-bon/d" $TCRON
+    sed -i -e "/bibbox-clear-bon-queue/d" $TCRON
     crontab $TCRON
   fi
 else
-  echo "30 7 * * * /home/superuser/bibbox-clear-bon-queue.sh" >> $TCRON
+  sed -i -e "/bibbox-clear-bon-queue/d" $TCRON
+  echo "* * * * * /home/superuser/bibbox-clear-bon-queue.sh" >> $TCRON
   crontab $TCRON
 fi
 
