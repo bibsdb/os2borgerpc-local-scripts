3f4c688253223e819557c0dcbb9568ca5ee2bd75
Agnete Moos <agms@sonderborg.dk>
2024-04-30 11:49:54 +0200

    Add Ricoh printers

---
+++ 2004printerindstillinger-RICOH-C3010.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Set default settings
+lpadmin -p RICOH-C3010 -o Resolution=1200dpi
+lpadmin -p RICOH-C3010 -o PageSize=A4
---
+++ 2004printerindstillinger-RICOH-C4510.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Set default settings
+lpadmin -p RICOH-C4510 -o Resolution=1200dpi
+lpadmin -p RICOH-C4510 -o PageSize=A4
